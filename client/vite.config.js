35860a77c1ea6c5dda2b0ca71f56c339f4c11415
Aniket Singh <27aniketsingh077@gmail.com>
2025-09-15 17:27:39 +0530

    Second commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [ react(),
-             tailwindcss(),
-          ],
+             tailwindcss(),],
+             build: {
+    outDir: 'dist', // this is default, fine
+    emptyOutDir: true,
+  },
+  base: './' // important for relative paths
+          
 })
